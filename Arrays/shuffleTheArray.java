a1d4e174c33cbebc0711eb0b1dd195ceb79c8df0
Prem Shandilya <you@example.com>
2026-01-16 06:10:46 +0530

    added a new code - Shuffle the Array problem.

---
+++ Arrays/shuffleTheArray.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+public class shuffleTheArray {
+  public static void main(String[] args) {
+    int[] nums = {1,2,3,4,5,6};
+    int n = 3;
+
+    int[] result = new int[nums.length];
+    int index = 0;
+
+    for(int i = 0; i < n; i++){
+      result[index++] = nums[i];
+      result[index++] = nums[i + n];
+    }
+    System.out.println(Arrays.toString(result));
+
+  }
+}
