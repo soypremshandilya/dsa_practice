c458e5d75085f01811e4c2b68fc26542d33101ac
Prem Shandilya <you@example.com>
2026-02-06 19:22:34 +0530

    added string rev using StringBuilder code.

---
+++ Strings/stringRev.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class stringRev {
+    public static void main(String[] args) {
+        String str = "Prem";
+        StringBuilder rev = new StringBuilder(str);
+        rev.reverse();
+        System.out.println(rev);
+  }
+}
